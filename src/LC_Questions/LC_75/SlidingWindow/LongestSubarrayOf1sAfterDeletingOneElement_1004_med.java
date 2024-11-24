3dd886b2052c748a464070bf4caa216d2a12285d
chen.shen <chen.shen@tgptelecom.com.au>
2024-11-25 10:41:32 +1100

    25/11/2024 sliding windows

--- src/LC_Questions/LC_75/SlidingWindow/LongestSubarrayOf1sAfterDeletingOneElement_1004_med.java
+++ src/LC_Questions/LC_75/SlidingWindow/LongestSubarrayOf1sAfterDeletingOneElement_1004_med.java
@@ -16,7 +16,7 @@ package LC_Questions.LC_75.SlidingWindow;
  * Output: 2
  * Explanation: You must delete one element.
  */
-public class LongestSubarrayOf1sAfterDeletingOneElement {
+public class LongestSubarrayOf1sAfterDeletingOneElement_1004_med {
     public int longestSubarray(int[] nums) {
         int index = 0, count = 0, ans = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -33,10 +33,30 @@ public class LongestSubarrayOf1sAfterDeletingOneElement {
         }
         return ans;
     }
+    public int longestOnesMySolutionTEL(int[] nums, int k) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int temp = 0, cur = 0;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] == 0) {
+                    if(temp == k) {
+                        break;
+                    }
+                    temp++;
+                    cur++;
+                } else {
+                    cur++;
+                }
+            }
+            max = Math.max(max, cur);
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
-        LongestSubarrayOf1sAfterDeletingOneElement l = new LongestSubarrayOf1sAfterDeletingOneElement();
-        int [] nums = {1,1,0,0,1,1,1,0,1};
-        System.out.println(l.longestSubarray(nums));
+        LongestSubarrayOf1sAfterDeletingOneElement_1004_med l = new LongestSubarrayOf1sAfterDeletingOneElement_1004_med();
+        int [] nums = {1,1,0,0,0,1,1,1,0,1};
+        int k = 2;
+        System.out.println(l.longestOnesMySolutionTEL(nums, k));
     }
 }
