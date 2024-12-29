83d077d9e954bcaf6ae4fb6fa055da60dfc4d277
chen.shen <chen.shen@tgptelecom.com.au>
2024-12-30 09:16:09 +1100

    30/12/2024 Update a lot

---
+++ src/LC_Questions/AtlanssianQ/RankTeamsByVotes_1366.java
@@ -0,0 +1,7 @@
+package LC_Questions.AtlanssianQ;
+
+public class RankTeamsByVotes_1366 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/LC_Questions/Google30days/HelloWorld_2667.java
@@ -0,0 +1,9 @@
+package LC_Questions.Google30days;
+
+public class HelloWorld_2667 {
+//    function createHelloWorld() {
+//        return function(): string {
+//            return "Hello World";
+//        };
+//    };
+}
