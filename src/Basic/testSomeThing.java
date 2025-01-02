e548529839dfe9c3095826f41df88b386670c3ac
chen.shen <chen.shen@tgptelecom.com.au>
2025-01-02 21:51:12 +1100

    02/01/2024 UpdatecountVowel, main, evenOdd swap

---
+++ src/Basic/testSomeThing.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class testSomeThing {
+    public static void main(String[] args) {
+        int [] sss = {1,2,3,4,5,6,7,8};
+        int [] newSss = new int [sss.length];
+        int l = 0, r = sss.length;
+
+        int index = 0;
+        int rindex = sss.length - 1;
+        for (int i = 0; i < sss.length ; i++) {
+            if (sss[i] % 2 != 0) {
+                newSss[index++] = sss[i];
+            } else {
+                newSss[rindex--] = sss[i];
+            }
+        }
+        for (int i = 0; i < newSss.length; i++) {
+            System.out.println(newSss[i]);
+        }
+    }
+}
