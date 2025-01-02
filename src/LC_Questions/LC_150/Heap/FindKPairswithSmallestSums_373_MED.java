cdcdf4be35711f9f6402f1d4d233bd90c0eff2b2
chen.shen <chen.shen@tgptelecom.com.au>
2025-01-03 09:15:13 +1100

    03/01/2024 Update findMedianFromData Stream. Not sure why the median means smallest with largest, but it's fine.

--- src/LC_Questions/LC_150/Heap/FindKPairswithSmallestSums_373_MED.java
+++ src/LC_Questions/LC_150/Heap/FindKPairswithSmallestSums_373_MED.java
@@ -66,15 +66,15 @@ public class FindKPairswithSmallestSums_373_MED {
         return ans;
     }
     public List<int[]> kSmallestPairsSingleRound(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> que = new PriorityQueue<>((a,b)->a[0]+a[1]-b[0]-b[1]);
+        PriorityQueue <int[]> q = new PriorityQueue<>((a,b)-> a[0] + a[1] - b[0] - b[1]);
         List<int[]> res = new ArrayList<>();
-        if(nums1.length==0 || nums2.length==0 || k==0) return res;
-        for(int i=0; i<nums1.length && i<k; i++) que.offer(new int[]{nums1[i], nums2[0], 0});
-        while(k-- > 0 && !que.isEmpty()){
-            int[] cur = que.poll();
-            res.add(new int[]{cur[0], cur[1]});
-            if(cur[2] == nums2.length-1) continue;
-            que.offer(new int[]{cur[0], nums2[cur[2]+1], cur[2]+1});
+        if (nums1.length==0 || nums2.length==0 || k==0) return res;
+        for (int i=0; i <nums1.length && i < k; i++) q.offer(new int[]{nums1[i], nums2[0], 0});
+        while (k-- > 0 && !q.isEmpty()){
+            int[] cur = q.poll();
+            res.add (new int[]{cur[0], cur[1]});
+            if (cur[2] == nums2.length-1) continue;
+            q.offer (new int[]{cur[0], nums2[cur[2]+1], cur[2]+1});
         }
         return res;
     }
