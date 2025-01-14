9336a538a3190924c53b5d1292b67a5c66f0ea31
chen.shen <chen.shen@tgptelecom.com.au>
2025-01-14 17:07:40 +1100

    14/01/2024 some updates

--- src/LC_Questions/DailyChanllenges/FindThePrefixCommonArrayOfTwoArrays_2657/HashSetMethod.java
+++ src/LC_Questions/DailyChanllenges/FindThePrefixCommonArrayOfTwoArrays_2657/HashSetMethod.java
@@ -5,36 +5,26 @@ import java.util.Set;
 
 public class HashSetMethod {
     public static void main(String[] args) {
-
+        int [] a = {1,3,2,4};
+        int [] b = {3,1,2,4};
+        System.out.println(findThePrefixCommonArray(a,b));
     }
-    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+    public static int[] findThePrefixCommonArray(int[] A, int[] B) {
         int n = A.length;
         int[] prefixCommonArray = new int[n];
-
-        // Initialize sets to store elements from A and B
         Set<Integer> elementsInA = new HashSet<Integer>();
         Set<Integer> elementsInB = new HashSet<Integer>();
-
-        // Iterate through the elements of both arrays
         for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
-            // Add current elements from A and B to respective sets
             elementsInA.add(A[currentIndex]);
             elementsInB.add(B[currentIndex]);
-
             int commonCount = 0;
-
-            // Count common elements between the sets
             for (int element : elementsInA) {
                 if (elementsInB.contains(element)) {
                     ++commonCount;
                 }
             }
-
-            // Store the count of common elements for the current prefix
             prefixCommonArray[currentIndex] = commonCount;
         }
-
-        // Return the final array with counts of common elements in each prefix
         return prefixCommonArray;
     }
 }
