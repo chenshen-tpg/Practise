d111f81784b66b690d1bc18b5030842241797e66
chen.shen <chen.shen@tgptelecom.com.au>
2025-04-12 13:41:14 +1000

    feat: add sliding window algorithms for finding K-length substrings without repeats and missing ranges

---
+++ src/LC_Questions/Array/Intervals/Others/MeetingRooms252/Review.java
@@ -0,0 +1,16 @@
+package LC_Questions.Array.Intervals.Others.MeetingRooms252;
+
+import java.util.Arrays;
+
+public class Review {
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals.length == 0 || intervals.length == 1) return true;
+        Arrays.sort(intervals, (int [] a, int [] b) -> a[0] - b[0]);
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) return false;
+            end = intervals[i][1];
+        }
+        return true;
+    }
+}
---
+++ src/LC_Questions/Array/SlidingWindow/PremiumAlgo100/FindKLengthSubSringsWithNoRepeated_1100/HashMapSliding.java
@@ -0,0 +1,4 @@
+package LC_Questions.Array.SlidingWindow.PremiumAlgo100.FindKLengthSubSringsWithNoRepeated_1100;
+
+public class HashMapSliding {
+}
