a0d596bdddbab97fe5ad51a4fc8daab416a06df8
chen.shen <chen.shen@tgptelecom.com.au>
2025-04-13 18:03:11 +1000

    feat: implement removeKDigits algorithm with multiple class implementations

---
+++ src/LC_Questions/Array/ArrayGeneral/Others/RemoveKDigits_402/TEST.java
@@ -0,0 +1,25 @@
+package LC_Questions.Array.ArrayGeneral.Others.RemoveKDigits_402;
+
+public class TEST {
+    public static void main(String[] args) {
+        TEST solution = new TEST();
+        solution.removeKDigits("33526221184202197273", 19);
+    }
+
+    public String removeKDigits(String num, int k) {
+        long minimal = Integer.MAX_VALUE;
+        for (int i = 0; i < num.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            for (int j = i; j < num.length(); j++) {
+                if (j + count == num.length()) break;
+                if (count++ < k) sb.append(num.charAt(j));
+            }
+            String tempString = num;
+            if (sb.toString().equals(tempString)) return "0";
+            long tempVal = Long.valueOf(tempString.replaceFirst(sb.toString(),""));
+            minimal = Math.min(minimal, tempVal);
+        }
+        return String.valueOf(minimal);
+    }
+}
