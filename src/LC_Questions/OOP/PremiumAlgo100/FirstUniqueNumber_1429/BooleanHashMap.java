590fd8c91f710e2ac84d9b2e7fad4494e024f3a2
chen.shen <chen.shen@tgptelecom.com.au>
2025-04-22 08:15:35 +1000

    feat: implement BooleanHashMap, BruteForce, and HashMapQueue classes for tracking first unique numbers

---
+++ src/LC_Questions/OOP/PremiumAlgo100/FirstUniqueNumber_1429/BooleanHashMap.java
@@ -0,0 +1,35 @@
+package LC_Questions.OOP.PremiumAlgo100.FirstUniqueNumber_1429;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class BooleanHashMap {
+    private Set<Integer> q = new LinkedHashSet<>();
+    private Map<Integer, Boolean> hm = new HashMap<>();
+
+    public BooleanHashMap(int[] nums) {
+        for (int num : nums) {
+            this.add(num);
+        }
+    }
+
+    public int showFirstUnique() {
+        if (!q.isEmpty()) {
+            return q.iterator().next();
+        }
+        return -1;
+    }
+
+    public void add(int value) {
+        if (!hm.containsKey(value)) {
+            hm.put(value, true);
+            q.add(value);
+        } else if (hm.get(value)) {
+            hm.put(value, false);
+            q.remove(value);
+        }
+    }
+
+}
