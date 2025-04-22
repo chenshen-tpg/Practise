8e3c975bd18aeb8eab61d90a2b543c5edd95614e
chen.shen <chen.shen@tgptelecom.com.au>
2025-04-22 20:31:56 +1000

    feat: implement BFS and DFS algorithms for pathfinding in a maze, and add plusOne method for linked list manipulation

--- src/LC_Questions/Hash/Others/CountVowelSubStringOfAString_2062/SlidingWindows.java
+++ src/LC_Questions/Hash/Others/CountVowelSubStringOfAString_2062/SlidingWindows.java
@@ -5,20 +5,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SlidingWindows {
+    public static void main(String[] args) {
+        SlidingWindows solution = new SlidingWindows();
+        solution.countVowelSubstrings("cuaieuouac");
+    }
     public int countVowelSubstrings(String word) {
         int count = 0;
-        Map<Character, Integer> lastSeen = new HashMap<>();
-        lastSeen.put('a', -1);
-        lastSeen.put('e', -1);
-        lastSeen.put('i', -1);
-        lastSeen.put('o', -1);
-        lastSeen.put('u', -1);
+        Map<Character, Integer> hm = new HashMap<>();
+        hm.put('a', -1);
+        hm.put('e', -1);
+        hm.put('i', -1);
+        hm.put('o', -1);
+        hm.put('u', -1);
 
         for (int i = 0, lastInvalidPos = -1; i < word.length(); ++i) {
-            if (lastSeen.containsKey(word.charAt(i))) {
-                lastSeen.put(word.charAt(i), i);
-                count += Math.max(Collections.min(lastSeen.values()) - lastInvalidPos, 0);
+            if (hm.containsKey(word.charAt(i))) {
+                hm.put(word.charAt(i), i);
+                //Otherwise the collections.min will just return - 1 if no encounter all
+                count += Math.max(Collections.min(hm.values()) - lastInvalidPos, 0);
             } else {
+                // if new invalid break
                 lastInvalidPos = i;
             }
         }
