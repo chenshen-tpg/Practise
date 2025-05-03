8ede24ec783a10c87c979e2c1609274501ef1b14
chen.shen <chen.shen@tgptelecom.com.au>
2025-05-03 11:23:33 +1000

    feat: add implementations for rook capture and power of four problems in new classes

---
+++ front/src/app/TEST.ts
@@ -0,0 +1,11 @@
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+  try {
+    const [res1, res2] = await Promise.all([promise1, promise2]);
+    return res1 + res2;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
