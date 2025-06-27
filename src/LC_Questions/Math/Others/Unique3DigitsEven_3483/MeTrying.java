bfe021bf89d29025f1d14e3976659b1ee03d2ed9
Chen.Shen <117608047+chenshen-tpg@users.noreply.github.com>
2025-06-27 11:02:33 +1000

    Three loops instead of having a total permutation.

---
+++ src/LC_Questions/Math/Others/Unique3DigitsEven_3483/MeTrying.java
@@ -0,0 +1,30 @@
+package LC_Questions.Math.Others.Unique3DigitsEven_3483;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class MeTrying {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int totalNumbers(int[] digits) {
+        HashSet<Integer> li = new HashSet<>();
+        int cnt = 0;
+        int n = digits.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (i != j && i != k && j != k && digits[i] != 0 && digits[k] % 2 == 0) {
+                        int cur = digits[i] * 100 + digits[j] * 10 + digits[k];
+                        li.add(cur);
+                    }
+                }
+            }
+        }
+        return li.size();
+    }
+}
