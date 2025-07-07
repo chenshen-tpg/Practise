ab4f241e5bbc18b3f70d1f3a061698d3c23b9163
Chen.Shen <117608047+chenshen-tpg@users.noreply.github.com>
2025-07-07 11:44:37 +1000

    feat: add ModelAns and SampleAns classes for Kth Missing Positive Number and string scoring

--- src/LC_Questions/Array/ArrayGeneral/Others/RepeatSubString_459/ModelAnswer2.java
+++ src/LC_Questions/Array/ArrayGeneral/Others/RepeatSubString_459/ModelAnswer2.java
@@ -4,6 +4,7 @@ public class ModelAnswer2 {
     public static void main(String[] args) {
 
     }
+
     public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
         for (int i = 1; i <= n / 2; i++) {
