171880fb30d40a3ddca07fa1bf702dd095f803b6
Chen.Shen <117608047+chenshen-tpg@users.noreply.github.com>
2025-07-23 08:03:31 +1000

    feat: rename RecursiveSolution to SampleAnsRec and add SampleAnsStack and SampleAnswerBest classes for calculator implementation

--- src/CodingQuestions/Stack/StackGeneral/LC150/BasicCalculator_224/SampleAnsRec.java
+++ src/CodingQuestions/Stack/StackGeneral/LC150/BasicCalculator_224/SampleAnsRec.java
@@ -1,13 +1,12 @@
 package CodingQuestions.Stack.StackGeneral.LC150.BasicCalculator_224;
 
-public class RecursiveSolution {
-    int idx;
-
+public class SampleAnsRec {
     public static void main(String[] args) {
 
     }
 
-    public int recursiveSolution(String s) {
+    int idx; // this index traverse the string in one pass, between different level of recursion
+    public int calculate(String s) {
         idx = 0; // Initialization should be here
         return calc(s);
     }
@@ -17,14 +16,14 @@ public class RecursiveSolution {
         while (idx < s.length()) {
             char c = s.charAt(idx++);
             if (c >= '0' && c <= '9') num = num * 10 + c - '0';
-            else if (c == '(') num = calc(s);
+            else if (c == '(') num = calc(s); // ( is start of a new sub-problem, Let recursion solve the sub-problem
             else if (c == ')') return res + sign * num;
-            else if (c == '+' || c == '-') {
+            else if (c == '+' || c == '-') { // only when we meet a new sign, we know a while number has been read
                 res += sign * num;
                 num = 0;
                 sign = c == '-' ? -1 : 1;
             }
         }
-        return res + sign * num;
+        return res + sign * num; // last number is not processed yet
     }
 }
