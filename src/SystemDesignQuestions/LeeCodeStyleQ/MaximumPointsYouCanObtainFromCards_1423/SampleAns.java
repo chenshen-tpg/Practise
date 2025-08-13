ca5430fe78600f45dacb2c047ea09d1117a9552e
Chen.Shen <117608047+chenshen-tpg@users.noreply.github.com>
2025-08-14 08:48:03 +1000

    feat: implement ProductFilter class to return top 5 products by sales volume; add test cases for various scenarios

---
+++ src/SystemDesignQuestions/LeeCodeStyleQ/MaximumPointsYouCanObtainFromCards_1423/SampleAns.java
@@ -0,0 +1,30 @@
+package SystemDesignQuestions.LeeCodeStyleQ.MaximumPointsYouCanObtainFromCards_1423;
+
+public class SampleAns {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxScore(int[] cards, int k) {
+        int total = 0;
+        for (int card : cards) {
+            total += card;
+        }
+        if (k >= cards.length) {
+            return total;
+        }
+        int temp = 0;
+        int max = 0;
+        int j = 0;
+        for (int i = 0; i < cards.length; i++) {
+            temp += cards[i];
+            if (i - j + 1 == cards.length - k) {
+                max = Math.max(total - temp, max);
+                temp -= cards[j++];
+            }
+        }
+
+        return max;
+    }
+}
