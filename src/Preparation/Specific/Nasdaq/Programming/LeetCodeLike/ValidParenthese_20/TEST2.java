80016b239003ff466187c9f8f29b4ddb5509dcaa
chen.shen <chen.shen@tgptelecom.com.au>
2025-09-10 08:59:21 +1000

    add SQL queries for processing time calculations and implement reverse words and valid parentheses solutions

---
+++ src/Preparation/Specific/Nasdaq/Programming/LeetCodeLike/ValidParenthese_20/TEST2.java
@@ -0,0 +1,25 @@
+package Preparation.Specific.Nasdaq.Programming.LeetCodeLike.ValidParenthese_20;
+
+import java.util.Stack;
+
+public class TEST2 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.add(c);
+            }
+            else {
+                if (stack.isEmpty()) return false;
+                char temp = stack.pop();
+                if (c == ')' && temp != '(') return false;
+                if (c == ']' && temp != '[') return false;
+                if (c == '}' && temp != '{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
