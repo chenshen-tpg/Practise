2c99d679f9fa9b6097d6f37683ea8e70835308f5
chen.shen <chen.shen@tgptelecom.com.au>
2025-09-10 18:51:37 +1000

    add SQL queries for friend requests and reported posts, and implement product of array except self and linked list cycle solutions

---
+++ src/Preparation/Specific/Nasdaq/Programming/LeetCodeLike/LinkedListCycle_141/TEST.java
@@ -0,0 +1,19 @@
+package Preparation.Specific.Nasdaq.Programming.LeetCodeLike.LinkedListCycle_141;
+
+import CodingQuestions.Lib.ListNode;
+
+public class TEST {
+    public static void main(String[] args) {
+
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
