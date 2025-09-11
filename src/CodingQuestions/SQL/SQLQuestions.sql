0ad685b61fd12c0e0575b78d33473014fd0ef6e3
chen.shen <chen.shen@tgptelecom.com.au>
2025-09-11 15:35:10 +1000

    add SQL query for finding customers with a referee ID other than '2' or no referee

--- src/CodingQuestions/SQL/SQLQuestions.sql
+++ src/CodingQuestions/SQL/SQLQuestions.sql
@@ -4,10 +4,7 @@ SELECT product_id FROM Products
 WHERE low_fats = 'Y' and recyclable = 'y';
 
 
--- 584. Find Customer Referee
-SELECT name FROM Customer WHERE referee_id <> '2' OR referee_id IS NULL;
 
--- 595. Big Countries
 
 SELECT name, population, area FROM World WHERE
     population >= 25000000  or  area >= 3000000;
