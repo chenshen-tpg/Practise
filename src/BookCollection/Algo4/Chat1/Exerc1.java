29209283072c6149fe127035a8ddb67c5f3bcbb8
chen.shen <chen.shen@tgptelecom.com.au>
2025-10-17 10:04:41 +1100

    17-10-2025 update things.
    Signed-off-by: Chen.Shen <117608047+chenshen-tpg@users.noreply.github.com>

---
+++ src/BookCollection/Algo4/Chat1/Exerc1.java
@@ -0,0 +1,16 @@
+package BookCollection.Algo4.Chat1;
+
+import java.util.Scanner;
+
+public class Exerc1 {
+
+    public static void method1() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
---
+++ src/BookCollection/Algo4/Chat1/Exerc3.java
@@ -0,0 +1,9 @@
+package BookCollection.Algo4.Chat1;
+
+public class Exerc3 {
+    public static void main(String[] args) {
+        double pi = 3.1415926535;
+        System.out.printf("%.5f\n", pi);
+
+    }
+}
