543c4c1ef0d422e08d8a8f0faaaeb9d5bae638d4
chen.shen <chen.shen@tgptelecom.com.au>
2026-01-26 12:05:29 +1100

    25/1/2025 update some old q

---
+++ src/CodingQuestions/Algorithms/NextStep/WeeklyContest_486/Q1_3818.java
@@ -0,0 +1,29 @@
+package CodingQuestions.Algorithms.NextStep.WeeklyContest_486;
+
+public class Q1_3818 {
+    public static void main(String[] args) {
+
+    }
+    public int minimumPrefixLength(int[] nums) {
+        int r = 0;
+        int i = nums.length -1;
+        while (i-1 >= 0 && nums[i - 1] < nums[i]) {
+            i--;
+            r++;
+        }
+        return r;
+    }
+
+    public int myVersion(int [] nums) {
+        int r = nums.length - 1;
+        int count = 0;
+        for (int i = r - 1; i > 0 ; i--) {
+            if (nums[i - 1] < nums[i]) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/CodingQuestions/Algorithms/NextStep/WeeklyContest_486/Q2_3819.java
@@ -0,0 +1,40 @@
+package CodingQuestions.Algorithms.NextStep.WeeklyContest_486;
+
+public class Q2_3819 {
+
+    public int[] rotateElements(int[] nums, int k) {
+        if (k == 0) return nums;
+        if (nums.length == 1) return nums;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                count++;
+            }
+        }
+        int[] memo = new int[count];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                memo[index++] = nums[i];
+            }
+        }
+        if (count == 0) return nums;
+        int rotate = k % count;
+        int newIndex = 0;
+        int[] newm = new int[count];
+        for (int i = rotate; i < index; i++) {
+            newm[newIndex++] = memo[i];
+        }
+        for (int i = 0; i<rotate; i++) {
+            newm[newIndex++] = memo[i];
+        }
+        int newStart = 0;
+        for (int i = 0; i < nums.length;i++) {
+            if(nums[i] >= 0) {
+                nums[i] = newm[newStart++];
+            }
+        }
+        return nums;
+    }
+
+}
